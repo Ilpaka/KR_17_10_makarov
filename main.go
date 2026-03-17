94a17ca8f57359cb549bdb5a1f8add583cd64766
ilpaka <ilyapanov07@mail.ru>
2026-03-17 13:26:37 +0300

    add POST handler

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func main() {
 
 	news := r.Group("")
 	{
-		news.GET("/products")
-		news.POST("/add_news")
-		news.DELETE("/new/:id")
+		news.GET("/products", get_products)
+		news.POST("/add_products", create_products)
+		news.DELETE("/products/:id")
 	}
 
 	r.Run(":8090")
@@ -32,4 +32,18 @@ func get_products(c *gin.Context) {
 	{
 		c.JSON(http.StatusOK, gin.H{"data": products})
 	}
+}
+
+func create_products(c *gin.Context) {
+	var product Products
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product.Id = int64(len(products) + 1)
+	products = append(products, product)
+
+	c.JSON(http.StatusCreated, product)
 }
