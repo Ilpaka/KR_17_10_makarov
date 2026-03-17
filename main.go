091d8e47210fcd3cb9d57de5389bf7db2e1e4c0e
ilpaka <ilyapanov07@mail.ru>
2026-03-17 17:32:24 +0300

    Add DELETE /products/:id handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -8,10 +9,10 @@ import (
 )
 
 type Products struct {
-	Id       int64
-	Name     string
-	Price    int
-	in_stock int
+	Id      int64  `json:"id"`
+	Name    string `json:"name"`
+	Price   int    `json:"price"`
+	InStock int    `json:"in_stock"`
 }
 
 var products []Products
@@ -22,8 +23,9 @@ func main() {
 	news := r.Group("")
 	{
 		news.GET("/products", get_products)
+		news.GET("/products/:id", get_products_by_id)
 		news.POST("/add_products", create_products)
-		news.DELETE("/products/:id", get_products_by_id)
+		news.DELETE("/products/:id", delete_product_by_id)
 	}
 
 	r.Run(":8090")
@@ -70,4 +72,28 @@ func get_products_by_id(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": get_new})
+}
+
+func delete_product_by_id(c *gin.Context) {
+	deleteID, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный формат id"})
+		return
+	}
+
+	idx := -1
+	for i := range products {
+		if products[i].Id == deleteID {
+			idx = i
+			break
+		}
+	}
+
+	if idx == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Продукт не найден"})
+		return
+	}
+
+	products = append(products[:idx], products[idx+1:]...)
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Продукт %d удалён", deleteID)})
 }
