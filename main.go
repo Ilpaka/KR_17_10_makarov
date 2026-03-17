ea76bc8efd8444f16f471c6cfd7ee8997df1e41c
ilpaka <ilyapanov07@mail.ru>
2026-03-17 13:33:05 +0300

    add handler get_prducts_by_id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,7 +23,7 @@ func main() {
 	{
 		news.GET("/products", get_products)
 		news.POST("/add_products", create_products)
-		news.DELETE("/products/:id")
+		news.DELETE("/products/:id", get_products_by_id)
 	}
 
 	r.Run(":8090")
@@ -46,4 +47,27 @@ func create_products(c *gin.Context) {
 	products = append(products, product)
 
 	c.JSON(http.StatusCreated, product)
+}
+
+func get_products_by_id(c *gin.Context) {
+	get_id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный формат id"})
+		return
+	}
+
+	var get_new *Products
+	for i := range products {
+		if products[i].Id == get_id {
+			get_new = &products[i]
+			break
+		}
+	}
+
+	if get_new == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Продукт не найден"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": get_new})
 }
